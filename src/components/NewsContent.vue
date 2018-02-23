fd95e42d42881b8fb725ea097065aaf7d36e21de
lianlianghui <2997223773@qq.com>
2018-02-23 11:43:53 +0800

    隐藏独立的凤凰标签

--- src/components/NewsContent.vue
+++ src/components/NewsContent.vue
@@ -87,12 +87,16 @@
           span.ifengLogo{
             img{
               width:16px !important;
-              display:none;
-
+              display:none !important;
             }
           }
         }
-
+        span.ifengLogo{
+          img{
+            width:16px !important;
+            display:none !important;
+          }
+        }
         p.detailPic{
           width:100%;
           border:1px solid #ccc;
