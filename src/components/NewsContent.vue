d6828c84b5ae8b7e84d5b99968e3e20801f11efc
liwens <915355753@qq.com>
2018-04-27 16:34:23 +0800

    热点推荐有列表版本

--- src/components/NewsContent.vue
+++ src/components/NewsContent.vue
@@ -1,244 +1,237 @@
 <template>
-  <div class="detail_container" >
-    <h1 class="title">{{ newsData.title }}</h1>
-    <time class="time">{{ newsData.time }}</time>
-    <article ref="art"></article>
-    <div id="ws-zl-dybanner263"></div>
-    <content-in-list></content-in-list>
+<div class="detail_container">
+  <h1 class="title">{{ newsData.title }}</h1>
+  <time class="time">{{ newsData.time }}</time>
+  <article ref="art"></article>
+  <div id="ws-zl-dybanner263"></div>
+  <content-in-list></content-in-list>
 
-  </div>
+</div>
 </template>
 
 <script>
-  import {requestNewContent} from '../api/requestNewsContent'
-  import {mapMutations,mapGetters} from 'vuex'
-  import * as types from '../store/mutation-types'
-  import lazy from '../common/js/myLazyload'
-  import { Toast,MessageBox } from 'mint-ui';
-  import ContentInList from './contentInList';
+import { requestNewContent } from '../api/requestNewsContent'
+import { mapMutations, mapGetters } from 'vuex'
+import * as types from '../store/mutation-types'
+import lazy from '../common/js/myLazyload'
+import { Toast, MessageBox } from 'mint-ui';
+// import ContentInList from './contentInList';
 
-  export default {
-    name: "news-content",
-    components: {
-      ContentInList
+
+export default {
+  name: "news-content",
+  components: {
+    ContentInList: () =>
+      import ('./contentInList')
+  },
+  created() {
+    //加载广告
+    // this.get_ad();
+
+    this.getContent();
+  },
+  data() {
+    return {
+      newsData: {},
+      lazyObj: null,
+      inputFouc: false,
+      isZan: false,
+      textinput: ''
+    }
+  },
+  mounted() {
+    this.check();
+  },
+  computed: {
+    ...mapGetters([
+      'contentData'
+    ])
+  },
+  watch: {
+    contentData: function(newData) {
+      this.newsData = newData;
+      this.$refs.art.innerHTML = newData.html.replace(/src/g, 'src = "" data-src');
+      this.hideCopy();
+      new lazy({
+        imgs: document.querySelectorAll('img')
+      });
+      this.$nextTick(() => {
+        this.setScrollTop();
+      })
     },
-    created() {
-      //加载广告
-      // this.get_ad();
-      
-      this.getContent();
-    },
-    data() {
-      return {
-        newsData: {},
-        lazyObj : null,
-        inputFouc: false,
-        isZan: false,
-        textinput: ''
-      }
-    },
-    mounted() {
-      this.check();
-    },
-    computed: {
-      ...mapGetters([
-        'contentData'
-      ])
-    },
-    watch: {
-      contentData:function(newData) {
-        console.log(newData);
-        this.newsData = newData;
-        this.$refs.art.innerHTML = newData.html.replace(/src/g, 'src = "" data-src');
-        this.hideCopy();
-        new lazy({
-          imgs:document.querySelectorAll('img')
-        });
-        this.$nextTick(()=> {
-          this.setScrollTop();
-        })
+    newsData: {
+      deep: true,
+      handler: function(newvalue) {
 
       }
-    },
-    methods: {
-      /**
-       * 检测低版本浏览器，并给出提示
-       * */
-      check() {
-        let nav = window.navigator;
-        let userAgent = nav.userAgent;
-        let v = userAgent.match(/Chrome\/(.*?)+/g);
-        if(v) {
-          v =  v[0].replace('Chrome/', "");
-          if(v) {
-            v = parseInt(v)
-            if(v < 50 && !sessionStorage.getItem('donshow')) {
-              MessageBox.confirm('检测到你的浏览器版本过低，如不能正常浏览，请升级或更换浏览器' +
-                '（点击取消，不再显示这条内容）').then(()=>{
-              }).catch((err)=> {
-               sessionStorage.setItem('donshow', 1)
-              })
-            }
+    }
+  },
+  methods: {
+    /**
+     * 检测低版本浏览器，并给出提示
+     * */
+    check() {
+      let nav = window.navigator;
+      let userAgent = nav.userAgent;
+      let v = userAgent.match(/Chrome\/(.*?)+/g);
+      if (v) {
+        v = v[0].replace('Chrome/', "");
+        if (v) {
+          v = parseInt(v)
+          if (v < 50 && !sessionStorage.getItem('donshow')) {
+            MessageBox.confirm('检测到你的浏览器版本过低，如不能正常浏览，请升级或更换浏览器' +
+              '（点击取消，不再显示这条内容）').then(() => {}).catch((err) => {
+              sessionStorage.setItem('donshow', 1)
+            })
           }
         }
-      },
-      /**
-       * 请求广告
-       * */
-      get_ad() {
-       return new Promise((resolve, reject)=> {
-         let adjs = document.createElement('script');
-         adjs.src='http://nads.wuaiso.com/newswap/wap/js/asyunion.js ';
-         document.body.appendChild(adjs);
-         adjs.onload = function () {
-           return resolve();
-         }
-        })
-      },
-      /**
-       * 修改第一个广告id
-       * */
-      change_ad_container_id() {
-        let last_ad_con =  document.getElementById('ws-zl-dybanner263');
-        let id = last_ad_con.id;
-        last_ad_con.setAttribute('id', id+1);
-      },
-      /**
-       * 生成广告容器。
-       * */
-      add_ad() {
-       let ad = document.createElement('div');
-       ad.setAttribute('id', 'ws-zl-dybanner263');
-       document.querySelector('.detail_container').appendChild(ad);
-       this.get_ad();
-      },
-      bindscroll() {
-        let scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
-        let windowHeight = window.innerHeight;
-        let bodyHeight = document.body.scrollHeight ||  document.documentElement.scrollHeight;
+      }
+    },
+    /**
+     * 请求广告
+     * */
+    get_ad() {
+      return new Promise((resolve, reject) => {
+        let adjs = document.createElement('script');
+        adjs.src = 'http://nads.wuaiso.com/newswap/wap/js/asyunion.js ';
+        document.body.appendChild(adjs);
+        adjs.onload = function() {
+          return resolve();
+        }
+      })
+    },
+    /**
+     * 修改第一个广告id
+     * */
+    change_ad_container_id() {
+      let last_ad_con = document.getElementById('ws-zl-dybanner263');
+      let id = last_ad_con.id;
+      last_ad_con.setAttribute('id', id + 1);
+    },
+    /**
+     * 生成广告容器。
+     * */
+    add_ad() {
+      let ad = document.createElement('div');
+      ad.setAttribute('id', 'ws-zl-dybanner263');
+      document.querySelector('.detail_container').appendChild(ad);
+      this.get_ad();
+    },
+    bindscroll() {
+      let scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+      let windowHeight = window.innerHeight;
+      let bodyHeight = document.body.scrollHeight || document.documentElement.scrollHeight;
 
-        // window.addEventListener('scroll', function () {
-        //
-        // })
-      },
+      // window.addEventListener('scroll', function () {
+      //
+      // })
+    },
 
-      /**
-       * 根据路由的params值，拿到新闻id发送请求获取新闻详细页
-       * */
-      getContent() {
-        let id = this.$route.params.id;
-        let params = {
-          id
-        };
-        //显示过度动画
-        this.set_loadingVis(true);
-        requestNewContent(params).then((res) => {
-          // this.newsData = res;
-          // console.log(res);
-          this.set_content_data(res);
-
-          this.$nextTick(() => {
-            this.$refs.art.innerHTML = res.html.replace(/src/g, 'src = "" data-src');
-            this.hideCopy();
-            //隐藏过度动画
-            this.set_loadingVis(false);
-            this.lazyObj = new lazy({
-              imgs:document.querySelectorAll('img')
-            });
-          })
-
-        }).catch((err) => {
-          alert('网络错误，错误代码:'+ err.toString())
-        })
-      },
-      setScrollTop() {
-        document.body.scrollTop = 0 +'px';
-        document.documentElement.scrollTop = 0 + 'px';
-        window.scrollTo(0,0);
-      },
-      hideCopy() {
-        let strong = document.querySelectorAll('strong');
-        let patt1 = new RegExp('凤凰网')
-        let patt2 = new RegExp('追究法律责任')
-        strong.forEach(el=> {
-          let ret1 = patt1.test(el.innerHTML);
-          let ret2 = patt2.test(el.innerHTML);
-          if(ret1 && ret2) {
-            el.parentNode.removeChild(el);
-          }
-        })
-      },
-      ...mapMutations({
-        set_loadingVis : types.SET_LOADING_VIS,
-        set_content_data: types.SET_CONTENT_DATA
-      }),
-    }
+    /**
+     * 根据路由的params值，拿到新闻id发送请求获取新闻详细页
+     * */
+    getContent() {
+      let id = this.$route.params.id;
+      let params = {
+        id
+      };
+      //显示过度动画
+      this.set_loadingVis(true);
+      requestNewContent(params).then((res) => {
+        this.set_content_data(res);
+      }).catch((err) => {
+        alert('网络错误，错误代码:' + err.toString())
+      })
+    },
+    setScrollTop() {
+      document.body.scrollTop = 0 + 'px';
+      document.documentElement.scrollTop = 0 + 'px';
+      window.scrollTo(0, 0);
+    },
+    hideCopy() {
+      let strong = document.querySelectorAll('strong');
+      let patt1 = new RegExp('凤凰网')
+      let patt2 = new RegExp('追究法律责任')
+      strong.forEach(el => {
+        let ret1 = patt1.test(el.innerHTML);
+        let ret2 = patt2.test(el.innerHTML);
+        if (ret1 && ret2) {
+          el.parentNode.removeChild(el);
+        }
+      })
+    },
+    ...mapMutations({
+      set_loadingVis: types.SET_LOADING_VIS,
+      set_content_data: types.SET_CONTENT_DATA
+    }),
   }
+}
 </script>
 
 <style  rel='stylesheet/scss' lang="scss">
-  @import "../common/sass/variable";
-  .detail_container {
-   padding: 5px 10px;
+@import "../common/sass/variable";
+.detail_container {
+    padding: 5px 10px;
 
-    .loading{
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
+    .loading {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
     }
     h1.title {
-      font-size: $font-size-large-x;
-      line-height: 26px;
-      margin: 10px 0;
+        font-size: $font-size-large-x;
+        line-height: 26px;
+        margin: 10px 0;
     }
-    time.time{
-      font-size:$font-size-small;
-      color:#b3b3b3;
+    time.time {
+        font-size: $font-size-small;
+        color: #b3b3b3;
     }
     article {
-      width: 100%;
-      text-align:left;
-      .yc_con_txt,.js_selection_area{
-        width:100%;
-        p{
-          width:100%;
-          font-size: $font-size-large;
-          line-height: 30px;
-          margin:18px auto;
-          font-weight: 400;
-          img{
-            width:100% !important;
-            /*margin: 0 !important;*/
-            margin: 5px 0;
-            height:auto;
+        width: 100%;
+        text-align: left;
+        .js_selection_area,
+        .yc_con_txt {
+            width: 100%;
+            p {
+                width: 100%;
+                font-size: $font-size-large;
+                line-height: 30px;
+                margin: 18px auto;
+                font-weight: 400;
+                img {
+                    width: 100% !important;
+                    /*margin: 0 !important;*/
+                    margin: 5px 0;
+                    height: auto;
 
-          }
-          span.ifengLogo{
-            img{
-              width:16px !important;
-              display:none !important;
+                }
+                span.ifengLogo {
+                    img {
+                        width: 16px !important;
+                        display: none !important;
+                    }
+                }
             }
-          }
+            span.ifengLogo {
+                img {
+                    width: 16px !important;
+                    display: none !important;
+                }
+            }
+            p.detailPic {
+                width: 100%;
+                margin: 10px auto;
+            }
+            p.picIntro {
+                text-align: center;
+                margin-top: 0;
+                font-size: $font-size-medium;
+                line-height: 18px;
+            }
         }
-        span.ifengLogo{
-          img{
-            width:16px !important;
-            display:none !important;
-          }
-        }
-        p.detailPic{
-          width:100%;
-          margin:10px auto;
-        }
-        p.picIntro{
-          text-align:center;
-          margin-top:0;
-          font-size:$font-size-medium;
-          line-height:18px;
-        }
-      }
     }
 
-  }
+}
 </style>
--- src/components/newsList.vue
+++ src/components/newsList.vue
@@ -1,7 +1,7 @@
 <template>
-  <section v-scroll>
-    <ul id="list_container">
-      <template v-for="(data, index) in curListData">
+<section v-scroll='aaaa'>
+  <ul id="list_container">
+    <template v-for="(data, index) in curListData">
         <!--ad-->
         <li v-if="index % 10 == 0 && index != 0" class="ad_list"><div id="ws-zl-dybanner263" ></div></li>
         <!--ad-->
@@ -15,228 +15,225 @@
           </div>
         </li>
       </template>
-    </ul>
-    <i class="loading" v-if="loadingIconVis">
+  </ul>
+  <i class="loading" v-if="loadingIconVis">
       <mt-spinner type="triple-bounce" color="rgb(38,162,255)"></mt-spinner>
     </i>
-    <mt-popup
-      v-model=noData
-      position="top"
-      popup-transition="popup-fade">
-      <div class="noData">没有数据了...</div>
-    </mt-popup>
-  </section>
+  <mt-popup v-model=noData position="top" popup-transition="popup-fade">
+    <div class="noData">没有数据了...</div>
+  </mt-popup>
+</section>
 </template>
 
 <script>
+import { requestNewList } from '../api/requestNewList'
+import { mapGetters, mapMutations } from 'vuex';
+import * as types from '../store/mutation-types'
 
-  import {requestNewList} from '../api/requestNewList'
-  import {mapGetters, mapMutations} from 'vuex';
-  import * as types from '../store/mutation-types'
-
-  export default {
-    directives: {
-      /**
-       * 滚动加载的自定义指令
-       */
-      scroll: {
-        bind: function (el, binding, vnode) {
-          window.addEventListener('scroll', vnode.context.scrollLoad)
-        },
-        unbind: function (el,binding, vnode) {
-          window.removeEventListener('scroll', vnode.context.scrollLoad)
-        }
-      }
-    },
-    name: "n_newslist",
-    data() {
-      return {
-        listdata: [],
-        page: 1,
-        noData: false,
-        loading: false,
-        loadingIconVis: false
-      }
-    },
-    watch: {
-      /**
-       * 记录当前频道
-       * */
-      curType: function (newType) {
-        this.initData();
-        this.getnewsData();
-      }
-    },
-    computed: {
-      ...mapGetters([
-        'curType',
-        'curListData',
-        'listPage'
-      ])
-    },
-    mounted() {
-      if (this.curListData.length == 0) {
-        this.getnewsData();
-      }
-    },
-    methods: {
-      /**
-       * 清除上一条广告的id
-       * */
-      remove_ad() {
-        let last_ad_con =  document.getElementById('ws-zl-dybanner263');
-        if(last_ad_con) {
-          last_ad_con.removeAttribute('id');
-        }
+export default {
+  directives: {
+    /**
+     * 滚动加载的自定义指令
+     */
+    scroll: {
+      bind: function(el, binding, vnode, oldVnode) {
+        window.addEventListener('scroll', vnode.context.scrollLoad)
       },
-      /**
-       * 加载广告
-       * */
-      get_ad() {
-          let adjs = document.createElement('script');
-          adjs.src='http://nads.wuaiso.com/newswap/wap/js/asyunion.js ';
-          document.body.appendChild(adjs);
-      },
-      scrollLoad() {
-        //滚动条高度（页面被卷去高度）
-        let scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
-        //文档高度
-        let bodyHeight = document.body.scrollHeight ||  document.documentElement.scrollHeight;
-
-        if (scrollTop + window.innerHeight >= bodyHeight - 50) {
-          //判断请求发送标志位，避免重复请求
-          if (this.loading) return;
-          //发送请求
-          this.getnewsData();
-        }
-      },
-      /**
-       * 前往新闻详细页，传入新闻ID值
-       * */
-      toNewsContent(id) {
-        sessionStorage.setItem('donLoading', 'don');
-        this.set_loadingVis(true);
-        this.$router.push({path: `/content/${id}`})
-      },
-      /**
-       * 切换频道时，重置vuex里记录的新闻列表数据和page
-       * */
-      initData() {
-        this.remove_curListData();
-        this.page = 1;
-      },
-      /**
-       * 请求数据
-       * */
-      getnewsData() {
-        let params = {
-          page: this.listPage,
-          type: this.curType
-        };
-        console.log(this.listPage)
-        if (this.curType == '') return
-        this.loading = true
-        this.loadingIconVis = true;
-        requestNewList(params).then((res) => {
-          //没有数据
-          if (res.length == 0) {
-            this.noData = true;
-            this.loadingIconVis = false;
-            setTimeout(() => {
-              this.noData = false;
-
-            }, 2000)
-            return;
-          }
-          this.set_curListData(res);
-          this.set_list_page()
-          this.loading = false;
-          this.loadingIconVis = false;
-          //加载广告
-             // this.remove_ad();
-             // this.get_ad();
-        })
-      },
-      ...mapMutations({
-        set_curListData: types.SET_CUR_LIST_DATA,
-        remove_curListData: types.REMOVE_CUR_LIST_DATA,
-        set_loadingVis : types.SET_LOADING_VIS,
-        set_list_page : types.SET_LIST_PAGE
-      }),
-      destroyed() {
-
+      unbind: function(el, binding, vnode) {
+        window.removeEventListener('scroll', vnode.context.scrollLoad)
       }
     }
+  },
+  name: "n_newslist",
+  data() {
+    return {
+      listdata: [],
+      page: 1,
+      noData: false,
+      loading: false,
+      loadingIconVis: false
+    }
+  },
+  watch: {
+    /**
+     * 记录当前频道
+     * */
+    curType: function(newType) {
+      this.initData();
+      this.getnewsData();
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'curType',
+      'curListData',
+      'listPage'
+    ])
+  },
+  mounted() {
+    if (this.curListData.length == 0) {
+      this.getnewsData();
+    }
+  },
+  methods: {
+    aaaa() {
+      return 'this is a'
+    },
+    /**
+     * 清除上一条广告的id
+     * */
+    remove_ad() {
+      let last_ad_con = document.getElementById('ws-zl-dybanner263');
+      if (last_ad_con) {
+        last_ad_con.removeAttribute('id');
+      }
+    },
+    /**
+     * 加载广告
+     * */
+    get_ad() {
+      let adjs = document.createElement('script');
+      adjs.src = 'http://nads.wuaiso.com/newswap/wap/js/asyunion.js ';
+      document.body.appendChild(adjs);
+    },
+    scrollLoad() {
+      //滚动条高度（页面被卷去高度）
+      let scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+      //文档高度
+      let bodyHeight = document.body.scrollHeight || document.documentElement.scrollHeight;
+
+      if (scrollTop + window.innerHeight >= bodyHeight - 50) {
+        //判断请求发送标志位，避免重复请求
+        if (this.loading) return;
+        //发送请求
+        this.getnewsData();
+      }
+    },
+    /**
+     * 前往新闻详细页，传入新闻ID值
+     * */
+    toNewsContent(id) {
+      sessionStorage.setItem('donLoading', 'don');
+      this.set_loadingVis(true);
+      this.$router.push({ path: `/content/${id}` })
+    },
+    /**
+     * 切换频道时，重置vuex里记录的新闻列表数据和page
+     * */
+    initData() {
+      this.remove_curListData();
+      this.page = 1;
+    },
+    /**
+     * 请求数据
+     * */
+    getnewsData() {
+      let params = {
+        page: this.listPage,
+        type: this.curType
+      };
+      console.log(this.listPage)
+      if (this.curType == '') return
+      this.loading = true
+      this.loadingIconVis = true;
+      requestNewList(params).then((res) => {
+        //没有数据
+        if (res.length == 0) {
+          this.noData = true;
+          this.loadingIconVis = false;
+          setTimeout(() => {
+            this.noData = false;
+
+          }, 2000)
+          return;
+        }
+        this.set_curListData(res);
+        this.set_list_page()
+        this.loading = false;
+        this.loadingIconVis = false;
+        //加载广告
+        // this.remove_ad();
+        // this.get_ad();
+      })
+    },
+    ...mapMutations({
+      set_curListData: types.SET_CUR_LIST_DATA,
+      remove_curListData: types.REMOVE_CUR_LIST_DATA,
+      set_loadingVis: types.SET_LOADING_VIS,
+      set_list_page: types.SET_LIST_PAGE
+    }),
+    destroyed() {
+
+    }
   }
+}
 </script>
 
 <style scoped rel='stylesheet/scss' lang="scss">
-  @import "../common/sass/variable";
+@import "../common/sass/variable";
 
-  .loading {
+.loading {
     text-align: center;
     position: fixed;
     bottom: 20px;
     left: 50%;
     transform: translateX(-50%);
-  }
+}
 
-  .noData {
+.noData {
     color: $color-theme;
     padding: 15px 10px;
     border-radius: 5px;
     background-color: $color-text-nd;
-  }
+}
 
- #list_container {
+#list_container {
     margin-top: $nav-height;
     width: 100%;
     overflow: auto;
     padding: 0 10px;
     box-sizing: border-box;
     li.detail_list {
-      width: 100%;
-      height: 100px;
-      display: flex;
-      justify-content: space-between;
-      border-bottom: 1px solid #eaeaea;
-      padding: 5px 0;
-      /*padding-top:20px;*/
-      .content {
-        max-width: 90%;
-        margin-top: 10px;
-        position: relative;
-        .title {
-          font-size: $font-size-large;
-          line-height: 22px;
-          height: 44px;
-          font-weight: 500;
-          overflow: hidden;
-          text-overflow: ellipsis;
-          word-break: break-all;
-          -webkit-box-orient: vertical;
-          -webkit-line-clamp: 2;
+        width: 100%;
+        height: 100px;
+        display: flex;
+        justify-content: space-between;
+        border-bottom: 1px solid #eaeaea;
+        padding: 5px 0;
+        /*padding-top:20px;*/
+        .content {
+            max-width: 90%;
+            margin-top: 10px;
+            position: relative;
+            .title {
+                font-size: $font-size-large;
+                line-height: 22px;
+                height: 44px;
+                font-weight: 500;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                word-break: break-all;
+                -webkit-box-orient: vertical;
+                -webkit-line-clamp: 2;
+            }
+            .time {
+                font-size: $font-size-small;
+                color: #b3b3b3;
+                line-height: 16px;
+                position: absolute;
+                bottom: 15px;
+            }
         }
-        .time {
-          font-size: $font-size-small;
-          color: #b3b3b3;
-          line-height: 16px;
-          position: absolute;
-          bottom: 15px;
+        .photo {
+            margin-left: 10px;
+            /*width: 85px;*/
+            /*height: 85px;*/
+            align-self: center;
+            img {
+                width: 110px;
+                height: 75px;
+            }
         }
-      }
-      .photo {
-        margin-left: 10px;
-        /*width: 85px;*/
-        /*height: 85px;*/
-        align-self: center;
-        img {
-          width: 110px;
-          height: 75px;
-        }
-      }
     }
-  }
-
-
+}
 </style>
